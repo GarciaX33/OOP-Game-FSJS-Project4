9fe584103cea8f7328348b47e581dec3c13bc9de
Daniel <danny.garcia80@hotmail.com>
2019-01-23 22:30:41 -0800

    will add function to add phrase to display

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,12 +2,28 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
-  /** declare the Phrase class. **/
+  /** will declare the Phrase class. **/
 
 class Phrase {
   /** Phrase class constructor method should receive one parameter: `phrase`.**/
   constructor(phrase) {
     this.phrase = phrase;
   }
+  /**
+  * will Display phrase on game board
+  */
+  addPhraseToDisplay() {
+    let thisPhrase = this.phrase.split('');
+    let thisUl = $('#phrase ul');
+    for (var i = 0; i < thisPhrase.length; i++) {
+      if (thisPhrase[i] === ' ') {
+        thisUl.append('<li class = "hide space"> </li>');
+      } else {
+        thisUl.append(`<li class = "hide letter ${thisPhrase[i]}">${thisPhrase[i]}</li>`);
+      }
+    }
+  }
+
+
 
 }
