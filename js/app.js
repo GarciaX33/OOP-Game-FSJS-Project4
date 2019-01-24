0fd3071e5e4af0dadd3245512bce56d1523e2ee9
Daniel <danny.garcia80@hotmail.com>
2019-01-23 22:34:43 -0800

    will edit app file to interact with button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-
 var phrases = [
      "the web app",
      "database",
@@ -10,3 +9,25 @@ var phrases = [
      "object oriented programming",
      "javascript library"
 ];
+
+const theGame = new Game(phrases);
+/** will hide main overlay once button is clicked  **/
+ $('#btn__reset').on('click',
+ function(){
+    hideOverlay();
+  });
+/** will hide the main overlay with hideOverlay function  **/
+function hideOverlay() {
+   $('#overlay').hide();
+   theGame.startGame();
+ }
+
+ // const logPhrase = (phrase) => {
+ // console.log(`Phrase - phrase: `, phrases[phrase]);
+ // };
+ // const game = new Game();
+ // logPhrase(game.getRandomPhrase());
+ // logPhrase(game.getRandomPhrase());
+ // logPhrase(game.getRandomPhrase());
+ // logPhrase(game.getRandomPhrase());
+ // logPhrase(game.getRandomPhrase());
